7629c031bc3f64a616a3e0c4d4262c1100c74304
Evan <dthung.y17@gmail.com>
2025-12-26 03:47:50 +0700

    init repo

---
+++ new-be/src/main/java/com/audisoft/audisoft/AudisoftApplication.java
@@ -0,0 +1,13 @@
+package com.audisoft.audisoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudisoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudisoftApplication.class, args);
+	}
+
+}
---
+++ new-be/src/test/java/com/audisoft/audisoft/AudisoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audisoft.audisoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudisoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
